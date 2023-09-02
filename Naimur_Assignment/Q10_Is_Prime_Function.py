97616a2d6191e5cfd27e84dee6e0d53e7fa268ea
naimur <mdnaimur45@gmail.com>
2023-09-02 16:45:07 +0600

    Assignment Updated

--- Naimur_Assignment/Q10_Is_Prime_Function.py
+++ Naimur_Assignment/Q10_Is_Prime_Function.py
@@ -1,12 +1,27 @@
-def is_prime(n):
-  for i in range(2,n):
-    if (n%i) == 0:
+def is_prime(numbers):
+  if numbers <= 1:
+    return False
+  if numbers == 2:
+    return True
+  if numbers % 2 == 0:
+    return False
+  for i in range(2, int(numbers**0.5) + 1):
+    if numbers % i == 0:
       return False
   return True
 
-start = int(input("starting value: "))
-end = int(input("ending value: "))
+while True:
+  try:
+    start = int(input("Enter the start of the range: "))
+    end = int(input("Enter the end of the range: "))
 
-for i in range(start, end+1):
-    if is_prime(i) == True:
-        print(i)
+    if start <= 2:
+      print(f"Prime numbers in the range {start} to {end}: ")
+    for num in range(start, end + 1):
+      if is_prime(num):
+        print(num)
+    break
+
+  except ValueError:
+    print("Invalid input. Please enter valid integers for the range.")
+        
--- Naimur_Assignment/Q4_Even_Odd_Divisibile.py
+++ Naimur_Assignment/Q4_Even_Odd_Divisibile.py
@@ -11,15 +11,15 @@ while True:
         except:
             user_input = input(f"'{user_input}' is a string. Please, Enter an integer Number: ")
 if number % 2 == 0:
-    print(f"{number} is an EVEN number")
+    print(f"{number} is an EVEN number.")
 else:
-    print(f"{number} is an ODD number")
+    print(f"{number} is an ODD number.")
 
 if number % 2 == 0 and number % 5 == 0:
-    print(f"{number} is divisible by 2 and 5")
+    print(f"{number} is divisible by 2 and 5.")
 elif number % 2 == 0:
-    print(f"{number} is only divisible by 2 not 5")
+    print(f"{number} is only divisible by 2 not 5.")
 elif number % 5 == 0:
-    print(f"{number} is only divisible by 5 not 2")
+    print(f"{number} is only divisible by 5 not 2.")
 else:
-    print(f"{number} is not divisible by 2 or 5")
+    print(f"{number} is not divisible by 2 or 5.")
